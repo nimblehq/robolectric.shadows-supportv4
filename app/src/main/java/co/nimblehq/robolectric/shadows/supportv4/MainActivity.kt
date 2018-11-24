693606e05f6d0bbb98ed38b690e616582a619dd6
Luong Vo <vodailuong@gmail.com>
2018-11-25 02:04:53 +0700

    migrate test app to androidx

--- app/src/main/java/co/nimblehq/robolectric/shadows/supportv4/MainActivity.kt
+++ app/src/main/java/co/nimblehq/robolectric/shadows/supportv4/MainActivity.kt
@@ -1,8 +1,8 @@
 package co.nimblehq.robolectric.shadows.supportv4
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
--- app/src/main/java/co/nimblehq/robolectric/shadows/supportv4/TestFragment.kt
+++ app/src/main/java/co/nimblehq/robolectric/shadows/supportv4/TestFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_test.*
 
 /**
  * Created by luongvo on 11/25/18.
