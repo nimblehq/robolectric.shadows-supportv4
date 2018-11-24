19e7c4dec5f572af00a1733db24f73bfc73d2338
Luong Vo <vodailuong@gmail.com>
2018-11-25 01:00:34 +0700

    init project

---
+++ app/src/main/java/co/nimblehq/robolectric/shadows/supportv4/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.nimblehq.robolectric.shadows.supportv4
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
